9313a29ee5b521d895abbeda6e2978c2edfc83e3
Nedas <budrysnedas@gmail.com>
2022-08-18 11:57:40 +0300

    dev branch + task 1 done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,30 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const formEl = document.forms[0];
+const inputEl = document.getElementById("search");
+const outputEl = document.getElementById("output");
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (!isNaN(inputEl.value)) printOutpustList();
+  else outputEl.innerHTML = '<p class="output">Please enter a number</p>';
+});
+
+function printOutpustList() {
+  outputEl.innerHTML = "";
+  const ulEl = document.createElement("ul");
+  for (let i = 0; i <= 2; i++) {
+    const liEl = document.createElement("li");
+    liEl.textContent = `Your converted weight amounts to: ${setLiText(i)}`;
+    ulEl.append(liEl);
+  }
+  outputEl.append(ulEl);
+  outputEl.className = "output";
+}
+
+function setLiText(iteration) {
+  if (iteration === 0) return `${(inputEl.value * 2.2046).toFixed(3)} lb.`;
+  if (iteration === 1) return `${(inputEl.value / 0.001).toFixed(3)} g.`;
+  if (iteration === 2) return `${(inputEl.value * 35.274).toFixed(3)} oz.`;
+}
